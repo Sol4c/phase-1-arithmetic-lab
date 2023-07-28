c9cc41c231e8dee5aba19f976b1b209c5e42c993
Sol4c <brownarmani86@yahoo.com>
2023-07-27 20:22:16 -0400

    math :(

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+const random = 8
+
+const num3 = 100
+const num4 = 6
+let mod = num3 % num4
+
+let max = (18, 19, 20);
+Math.max(max)
